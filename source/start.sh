7ab702a2ef13217ab1d2d0dd990051871071623a
arenasys <arenatemp@mailfence.com>
2024-07-28 15:56:13 +0800

    register to linux start menu

--- source/start.sh
+++ source/start.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-if [ ! -d "./source" ] 
-then
-    cd ..
-fi
+SCRIPT=$(realpath "$0")
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+
+cd "$SCRIPT_DIR"
+
+echo "[Desktop Entry]
+Exec=$SCRIPT %u
+Name=Lineworks
+Icon=$SCRIPT_DIR/launcher/icon.png
+MimeType=application/x-lineworks;x-scheme-handler/lineworks;
+Type=Application
+StartupNotify=false
+Terminal=false" > Lineworks-handler.desktop
+xdg-desktop-menu install Lineworks-handler.desktop
+xdg-mime default Lineworks-handler.desktop x-scheme-handler/lineworks
+rm Lineworks-handler.desktop
+chmod +x $SCRIPT
+
+cd ..
 
 if [ ! -d "./python" ] 
 then
-    echo "DOWNLOADING PYTHON..."
-    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-x86_64_v3-unknown-linux-gnu-install_only.tar.gz" -o "python.tar.gz"
+    flags=$(grep flags /proc/cpuinfo)
+    arch="x86_64"
+    if [[ $flags == *"sse4"* ]]; then
+        arch="x86_64_v2"
+    fi
+    if [[ $flags == *"avx2"* ]]; then
+        arch="x86_64_v3"
+    fi
+    if [[ $flags == *"avx512"* ]]; then
+        arch="x86_64_v4"
+    fi
+    echo "DOWNLOADING PYTHON ($arch)..."
+    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-$arch-unknown-linux-gnu-install_only.tar.gz" -o "python.tar.gz"
     
     echo "EXTRACTING PYTHON..."
     tar -xf "python.tar.gz"
     rm "python.tar.gz"
 fi
-./python/bin/python3 source/launch.py
+./python/bin/python3 source/launch.py "$@"
